5ecfe8d58cb986dbbd03a5fbe0a7c553d0a8b330
leeyena <20223126@gs.cwnu.ac.kr>
2025-03-20 11:18:50 +0900

    1.2.7 업데이트

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ module1 = cythonize([
 
 setup(
   name='nltkor',
-  version='1.2.6',
+  version='1.2.7',
 	url='https://modi.changwon.ac.kr/air_cwnu/nlp_tool/nltk_ko.git',
   packages=find_packages(exclude=[]),
   python_requires='>=3.7',
