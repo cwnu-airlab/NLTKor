7e1de563650c5e933efbf831e581388d5e0b9ac9
leeyena <20223126@gs.cwnu.ac.kr>
2025-03-24 15:51:56 +0900

    init 수정

--- nltkor/__init__.py
+++ nltkor/__init__.py
@@ -9,8 +9,8 @@ from nltkor import search
 from nltkor import similarity
 from nltkor import tag
 from nltkor import tokenize
-from nltkor import dataloader
-from nltkor.dataloader import dataset
 from nltkor import trans
 from nltkor import Kor_char
 from nltkor import etc
+
+__version__ = '1.2.8'
