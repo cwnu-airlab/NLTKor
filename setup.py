7171a7b30ddaadc4601714746832dad4901a411e
chanhyeok <chanhyeok@chanhyeokui-iMac.local>
2025-05-22 20:03:08 +0900

    version Update

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ module1 = cythonize([
 
 setup(
   name='nltkor',
-  version='1.2.18',
+  version='1.2.19',
 	url='https://modi.changwon.ac.kr/air_cwnu/nlp_tool/nltk_ko.git',
   packages=find_packages(exclude=[]),
   python_requires='>=3.7',
